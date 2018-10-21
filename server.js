c6de2ef6e19d30970981c918db330e101386e406
sojurner <pykim0591@gmail.com>
2018-10-20 21:14:21 -0600

    Inital Commit

---
+++ server.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+const cors = require('cors');
+const { apiKey } = require('./resources/apiKey');
+var port = 3001;
+
+// Configure app to use bodyParser to parse json data
+var app = express();
+app.use(cors());
+var server = require('http').createServer(app);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Test server is working (GET http://localhost:3001/api)
+app.get('/api/', function(req, res) {
+  res.json({ message: 'Hi, welcome to the server api!' });
+});
+
+var url_prefix = `https://api.darksky.net/forecast/${apiKey}/`;
+app.get('/api/darksky', function(req, res) {
+  try {
+    // Retrieves location coordinates (latitude and longitude) from client request query
+    var coordinates = req.query.latitude + ',' + req.query.longitude;
+    var url = url_prefix + coordinates;
+    console.log('Fetching ' + url);
+
+    fetch(url)
+      .then(function(response) {
+        if (response.status != 200) {
+          res
+            .status(response.status)
+            .json({ message: 'Bad response from Dark Sky server' });
+        }
+        return response.json();
+      })
+      .then(function(payload) {
+        res.status(200).json(payload);
+      });
+  } catch (err) {
+    console.log('Errors occurs requesting Dark Sky API', err);
+    res
+      .status(500)
+      .json({ message: 'Errors occurs requesting Dark Sky API', details: err });
+  }
+});
+
+server.listen(port);
+console.log('Server is listening on port ' + port);
